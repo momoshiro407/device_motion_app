c16ac184dbe17e699cf6bd7bceb5a19e340d7725
momoshiro407 <rozabmmsr@gmail.com>
2023-02-01 18:49:54 +0900

    ボタンクリックによる許可

--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
-$(function() {
-  if (window.DeviceMotionEvent && window.DeviceMotionEvent.requestPermission) {
+$(function () {
+  $('#confirm-button').on('click', () => {
+    useDeviceMotion();
+  });
+});
+
+function useDeviceMotion() {
+  if (DeviceMotionEvent && DeviceMotionEvent.requestPermission) {
     // デバイスの加速度センサーへのアクセス許可を確認する必要がある
     DeviceMotionEvent.requestPermission()
       .then(state => {
         // アクセスを許可する場合のみ実行可能
-        console.log(state);
         if (state === 'granted') {
           // 方向の計測結果を取得
-          $(document).on('deviceorientation', event => {
+          window.addEventListener("deviceorientation", event => {
             $('#orientation').text(`X: ${precision(event.beta)} / Y: ${precision(event.gamma)} / Z: ${precision(event.alpha)}`);
           });
           // 加速度の計測結果を取得
-          $(document).on('devicemotion', event => {
+          window.addEventListener('devicemotion', event => {
             const accel = event.acceleration;
             const accelG = event.accelerationIncludingGravity;
             const rotation = event.rotationRate;
             $('#acceleration').text(`X: ${precision(accel.x)} / Y: ${precision(accel.y)} / Z: ${precision(accel.z)}`);
             $('#acceleration-gravity').text(`X: ${precision(accelG.x)} / Y: ${precision(accelG.y)} / Z: ${precision(accelG.z)}`);
-            $('#rotation-rate').text(`X: ${precision(rotation.beta)} / Y: ${precision(rotation.gamma)} / Z: ${precision(rotation)}`);
+            $('#rotation-rate').text(`X: ${precision(rotation.beta)} / Y: ${precision(rotation.gamma)} / Z: ${precision(rotation.alpha)}`);
           });
         }
       })
+      .catch(error => console.log(error));
+  } else {
+    alert('このブラウザではDeviceMotionを利用できません。');
   }
+}
 
-  function precision(num) {
-    return num.toPrecision(4);
-  }
-});
+function precision(num) {
+  return num.toFixed(1);
+}
